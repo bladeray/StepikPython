eb14623ae2d335641720a049642df7e297cd1983
Eugene <raycut@yandex.ru>
2020-11-24 15:58:31 +0400

    tasks from first part and some from second part

---
+++ part1/lesson6_step10_notMine.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+import time
+
+try: 
+    link = "http://suninjuly.github.io/registration2.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    # 
+    input1 = browser.find_element_by_class_name('first')
+    input1.send_keys("Ivan")
+    input2 = browser.find_element_by_class_name('second')
+    input2.send_keys("Petrov")
+    input3 = browser.find_element_by_class_name('third')
+    input3.send_keys("Smolensk")
+
+    elements = list(filter(lambda el : el.get_attribute("required"), browser.find_elements_by_css_selector('input')))
+
+    assert len(elements) == 3
+
+    #
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+    # 
+    # 
+    time.sleep(1)
+
+    # 
+    welcome_text_elt = browser.find_element_by_tag_name("h1")
+    # 
+    welcome_text = welcome_text_elt.text
+
+    # 
+    assert "Congratulations! You have successfully registered!" == welcome_text
+
+finally:
+    # 
+    time.sleep(10)
+    # 
+    browser.quit()
+
+#
