dc2501d5ee2372e52e134f6c4dc3a4a3cc8d9878
Eugene <raycut@yandex.ru>
2020-12-02 21:17:05 +0400

    task with different language

--- part3/lesson6_step9/test_items.py
+++ part3/lesson6_step9/test_items.py
@@ -1,8 +1,10 @@
-from time import sleep
+import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link(browser):
+def test_add_to_basket_button_is_displayed(browser):
     browser.get(link)
-    sleep(10)
+    time.sleep(30)
+    assert browser.find_element_by_css_selector(".btn-add-to-basket").is_displayed()
+
